48a77798a4d4e759da7e62eae309a2aaee775bff
Ehsan Faramarzi <netpro@email.com>
2024-05-01 10:57:03 +0200

    names

--- Nik.Authorization.Models.Db/ServicesExtensions.cs
+++ Nik.Authorization.Models.Db/ServicesExtensions.cs
@@ -5,7 +5,7 @@ public static class ServicesExtensions
     private const string AuthorizationConnectionSettingsName = "AuthorizationMongoDB";
     private const string AuthorizationDbSettingsName = "DatabaseSettings:DatabaseName";
 
-    public static IServiceCollection ConfigureAuthorizationMongoDb(this IServiceCollection services)
+    public static IServiceCollection ConfigureNikAuthorizationMongoDb(this IServiceCollection services)
     {
         var mongoConnectionString = Context.Configuration.GetConnectionString(AuthorizationConnectionSettingsName);
         var databaseName = Context.Configuration[AuthorizationDbSettingsName];
--- Nik.Authorization/ServicesExtensions.cs
+++ Nik.Authorization/ServicesExtensions.cs
@@ -2,7 +2,7 @@
 
 public static class ServicesExtensions
 {
-    public static IServiceCollection AddNiCAuthorization(this IServiceCollection services)
+    public static IServiceCollection AddNikAuthorization(this IServiceCollection services)
     {
         services.AddScoped<IUserLoader, UserLoader>();
         services.AddScoped<IAuthorizer, Authorizer>();
