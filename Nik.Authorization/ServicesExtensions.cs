4e3b714d1648a7925b7cd2f8bf8aefcb76b42518
Ehsan Faramarzi <netpro@email.com>
2024-05-01 10:27:41 +0200

    ...

--- Nik.Authorization/ServicesExtensions.cs
+++ Nik.Authorization/ServicesExtensions.cs
@@ -1,10 +1,6 @@
-﻿[assembly: InternalsVisibleTo("Nik.Authorization.Seeder")]
-[assembly: InternalsVisibleTo("Nik.Authorization.Api")]
-[assembly: InternalsVisibleTo("Nik.Authorization.UnitTests")]
+﻿namespace Nik.Authorization;
 
-namespace Nik.Authorization;
-
-internal static class ServicesExtensions
+public static class ServicesExtensions
 {
     public static IServiceCollection AddNiCAuthorization(this IServiceCollection services)
     {
